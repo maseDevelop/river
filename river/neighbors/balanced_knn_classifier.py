3976730e9749c81e6271da49a71648059077818e
Mason <mason.elliott12@gmail.com>
2021-03-25 12:04:46 +0100

    started work on model

--- river/neighbors/balanced_knn_classifier.py
+++ river/neighbors/balanced_knn_classifier.py
@@ -1,16 +1,19 @@
 
-from . import KNNClassifier
+from .knn_classifier import KNNClassifier
+from .base_neighbors import BaseNeighbors
 from river import base
 import itertools
 import typing
 
 import numpy as np
 from scipy.spatial import cKDTree
+from river.utils import dict2numpy
+from river.utils.math import softmax
 
 from river import base
 from river.utils.skmultiflow_utils import get_dimensions
 
-class BalancedKNNClassifier(KNNClassifier):
+class BalancedKNNClassifier(KNNClassifier,BaseNeighbors):
     def __init__(self,n_neighbors=5,max_window_size=1000,leaf_size=30,metric='euclidean',classes=None):
         super().__init__(n_neighbors=n_neighbors, max_window_size=max_window_size,leaf_size=leaf_size,metric=metric)
         self.data_window = KNeighborsBalancedBuffer(window_size=max_window_size,classes=classes)
@@ -21,8 +24,51 @@ class BalancedKNNClassifier(KNNClassifier):
 
 
     def predict_proba_one(self, x):
-        # TODO:
-        raise NotImplementedError
+        """Predict the probability of each label for a dictionary of features `x`.
+
+        Parameters
+        ----------
+        x
+            A dictionary of features.
+
+        Returns
+        -------
+        proba
+            A dictionary which associates a probability which each label.
+
+        """
+
+        proba = {class_idx: 0.0 for class_idx in self.classes_}
+        if self.data_window.size == 0:
+            # The model is empty, default to None
+            return proba
+
+        x_arr = dict2numpy(x)
+
+        dists, neighbor_idx = self._get_neighbors(x_arr)
+        target_buffer = self.data_window.targets_buffer
+
+        # If the closest neighbor has a distance of 0, then return it's output
+        if dists[0][0] == 0:
+            proba[target_buffer[neighbor_idx[0][0]]] = 1.0
+            return proba
+
+        if self.data_window.size < self.n_neighbors:  # Select only the valid neighbors
+            print(neighbor_idx)
+            neighbor_idx = [index for cnt, index in enumerate(neighbor_idx[0])if cnt < self.data_window.size] #only get the valid number that are in the window
+            dists = [dist for cnt, dist in enumerate(dists[0]) if cnt < self.data_window.size]
+        else:
+            neighbor_idx = neighbor_idx[0]
+            dists = dists[0]
+
+        if not self.weighted:  # Uniform weights
+            for index in neighbor_idx:
+                proba[target_buffer[index]] += 1.0
+        else:  # Use the inverse of the distance to weight the votes
+            for d, index in zip(dists, neighbor_idx):
+                proba[target_buffer[index]] += 1.0 / d
+
+        return softmax(proba)
 
 #Need to work on indexing into array where classes are not zero and one
 class KNeighborsBalancedBuffer:
@@ -51,14 +97,10 @@ class KNeighborsBalancedBuffer:
 
         return self
 
-    def test_print(self):
-        print("X: ",self._X)
-        print("Y: ",self._y)
-
     def _configure(self):# Binary instance mask to filter data in the buffer
         self._imask = np.zeros([self.classes, self.window_size], dtype=bool)
         self._X = np.zeros((self.classes, self.window_size, self._n_features))
-        self._y = np.zeros([self.classes, self.window_size], dtype=bool)
+        self._y = np.zeros([self.classes, self.window_size], dtype=integer)
         self._is_initialized = True
 
     def append(self, x:np.ndarray, y: base.typing.Target):
@@ -68,9 +110,6 @@ class KNeighborsBalancedBuffer:
         y
             The target data for a single sample.
         """
-
-        print(x)
-        print(y)
 
         if not self._is_initialized:
             self._n_features = get_dimensions(x)[1]
@@ -96,7 +135,7 @@ class KNeighborsBalancedBuffer:
         if (slot_replaced):  # The oldest sample was replaced (complete cycle in the buffer)
             self._oldest = self._next_insert[self.classes]
         else:  # Actual buffer increased
-            self._size += 1
+            self._size += 1 #Not used after size == window_size
 
         return self
 
